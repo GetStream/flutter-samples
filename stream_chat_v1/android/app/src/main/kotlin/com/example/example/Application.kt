3bdb944f3b06ce7577eae2f668754aa23e7eccd6
Salvatore Giordano <salvatoregiordanoo@gmail.com>
2021-02-03 11:42:44 +0100

    cleanup

--- stream_chat_v1/android/app/src/main/kotlin/com/example/example/Application.kt
+++ stream_chat_v1/android/app/src/main/kotlin/com/example/example/Application.kt
@@ -4,15 +4,12 @@ import com.dexterous.flutterlocalnotifications.FlutterLocalNotificationsPlugin
 import io.flutter.app.FlutterApplication
 import io.flutter.plugin.common.PluginRegistry
 import io.flutter.plugin.common.PluginRegistry.PluginRegistrantCallback
-import io.flutter.plugins.firebasemessaging.FirebaseMessagingPlugin
-import io.flutter.plugins.firebasemessaging.FlutterFirebaseMessagingService
 import io.flutter.plugins.sharedpreferences.SharedPreferencesPlugin
 import io.flutter.plugins.pathprovider.PathProviderPlugin
 
 class Application : FlutterApplication(), PluginRegistrantCallback {
     override fun onCreate() {
         super.onCreate()
-        FlutterFirebaseMessagingService.setPluginRegistrant(this)
     }
 
     override fun registerWith(registry: PluginRegistry?) {
@@ -22,6 +19,5 @@ class Application : FlutterApplication(), PluginRegistrantCallback {
                 "io.flutter.plugins.sharedpreferences.SharedPreferencesPlugin"))
         FlutterLocalNotificationsPlugin.registerWith(registry?.registrarFor(
                 "com.dexterous.flutterlocalnotifications.FlutterLocalNotificationsPlugin"))
-        FirebaseMessagingPlugin.registerWith(registry?.registrarFor("io.flutter.plugins.firebasemessaging.FirebaseMessagingPlugin"))
     }
 }
