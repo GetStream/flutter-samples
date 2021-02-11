458fad23c7767aa459396adfb1909c31b74dd75d
Sacha <sacha.arbonel@hotmail.fr>
2021-02-11 19:32:56 +0100

    remove size required ChannelNameText

--- imessage/lib/channel_name_text.dart
+++ imessage/lib/channel_name_text.dart
@@ -2,8 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:stream_chat_flutter/stream_chat_flutter.dart' show Channel;
 
 class ChannelNameText extends StatelessWidget {
-  const ChannelNameText(
-      {Key key, @required this.channel, @required this.size = 12})
+  const ChannelNameText({Key key, @required this.channel, this.size = 12})
       : super(key: key);
 
   final Channel channel;
