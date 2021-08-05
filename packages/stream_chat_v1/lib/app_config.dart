33486afd0d731be7b07a565fee5e775e64b4cf9d
Fabrizio Moscon <mosconfabrizio@gmail.com>
2021-08-05 18:42:27 +0100

    feat: use 90poe api Key

--- packages/stream_chat_v1/lib/app_config.dart
+++ packages/stream_chat_v1/lib/app_config.dart
@@ -1,6 +1,6 @@
 import 'package:stream_chat_flutter/stream_chat_flutter.dart';
 
-const kDefaultStreamApiKey = 'kv7mcsxr24p8';
+const kDefaultStreamApiKey = 'em6azxphbfs9';
 
 final defaultUsers = <String, User>{
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoic2FsdmF0b3JlIn0.pgiJz7sIc7iP29BHKFwe3nLm5-OaR_1l2P-SlgiC9a8':
